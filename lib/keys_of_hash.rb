3dd2f5824f5ea7bac2bfb478b93614022c7414d4
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 13:20:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,12 @@ require 'pp'
 
 class Hash
   def keys_of(*arguments)
+    a = []
     self.each do |key, val|
-      
+      arguments.each do |arg|
+        a << key if val == arg 
+      end 
     end
+    a
   end
 end
